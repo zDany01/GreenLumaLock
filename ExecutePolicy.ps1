38902fada0615527b7aaf8e9bac50c539177968b
zDany01 <85403430+zDany01@users.noreply.github.com>
2025-11-26 17:25:54 +0100

    monitor parent process using .NET calls
    using .NET the script will only wait for the parent process

--- ExecutePolicy.ps1
+++ ExecutePolicy.ps1
@@ -1,6 +1,15 @@
-$exitcode = (Start-Process -FilePath "<GreenLumaLock full path>" -ArgumentList "noninteractive" -WorkingDirectory "<GreenLumaLock directory path>" -PassThru -Wait).ExitCode
-switch ($exitcode) {
-    15 {throw "Invalid GreenLuma state\nCheck status and lock file manually."}
+$GreenLumaLock = New-Object System.Diagnostics.ProcessStartInfo
+$GreenLumaLock.FileName = "<GreenLumaLock full path>"
+$GreenLumaLock.WorkingDirectory = "<GreenLumaLock dir path>"
+$GreenLumaLock.UseShellExecute = $true
+$GreenLumaLock.Arguments = "noninteractive"
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $GreenLumaLock
+$p.Start()
+$p.WaitForExit()    
+
+switch ($p.ExitCode) {
+    15 {throw "Invalid GreenLuma state! Check status and lock file manually."}
     32 {exit}
-    63 {throw "Internal Error\nCheck starter status" }
+    63 {throw "Internal Error! Check starter status" }
 }
